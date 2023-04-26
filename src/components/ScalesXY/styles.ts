0ed9e2f1a3ca59c297fa372cd276507d22b78df6
HARVEYNASH\tuyencaok <caokimtuyen1722@gmail.com>
2023-04-26 10:39:09 +0700

    update chart

--- src/components/ScalesXY/styles.ts
+++ src/components/ScalesXY/styles.ts
@@ -1,9 +1,10 @@
 import { ANNOTATIONS } from "../../constants/chartColor";
 
+
 export const line = {
   position: "absolute",
-  width: "80%",
-  left: "15%",
+  right: "3px",
+  left: "30%",
   borderTop: `5px solid ${ANNOTATIONS.MAIN_COLOR}`,
   bottom: "0",
   "&:before": {
@@ -18,8 +19,8 @@ export const line = {
 
 export const lineVertical = {
   position: "absolute",
-  height: "75%",
-  width: "1px",
+  width: "0",
+  bottom: "25%",
   left: "0",
   top: "0",
   borderRight: `5px solid ${ANNOTATIONS.MAIN_COLOR}`,
@@ -28,8 +29,8 @@ export const lineVertical = {
     fontWeight: "bold",
     position: "absolute",
     content: "'Opportunity'",
-    left: "-40px",
-    top: "15%",
+    right: "-40px",
+    top: "50%",
     transform: "rotate(270deg)",
   },
 };
