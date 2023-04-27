81b1032cab24a9dde00b46a0758bd967362e358a
HARVEYNASH\tuyencaok <caokimtuyen1722@gmail.com>
2023-04-27 13:11:28 +0700

    Update

---
+++ src/components/Setting/styles.ts
@@ -0,0 +1,23 @@
+export const style = {
+    position: "absolute" as "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 600,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+};
+
+export const lineStyle = {
+    display: "flex",
+    alignItems: "center",
+    padding: "0 0 20px",
+    justifyContent: "space-between",
+
+};
+
+export const colorPickerStyled = {
+    width: 30
+}
